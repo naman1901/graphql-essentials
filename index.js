3ebbe44566f0aef422db106c4d5dfe6043a8b08f
Naman Maheshwari <namahesh@microsoft.com>
2019-01-18 14:49:20 +0530

    Initial GraphQL Setup

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express'
+import graphqlHTTP from 'express-graphql';
+import schema from './schema'
 
 const app = express()
 
-app.get('/graphql', (req, res) => {
+app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
-})
+});
+
+const root = { hello: () => "Hi, I'm Naman" };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
 
 app.listen(8080, () => console.log('Running server on localhost:8080/graphql'));
