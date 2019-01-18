0894b5b3c46d02ca7a99153b6d5c64104f43d11b
Naman Maheshwari <naman1901@gmail.com>
2019-01-18 15:38:46 +0530

    Query and Mutation types

--- index.js
+++ index.js
@@ -8,20 +8,39 @@ app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
 
-const root = { friend: () => {
-    return {
-        "id": 1223,
-        "firstName": "Naman",
-        "lastName": "Maheshwari",
-        "gender": "Male",
-        "language": "English",
-        "emails": [
-            { "email": "placeholder1@emailplaceholder.com" },
-            { "email": "placeholder2@emailplaceholder.com" },
-            { "email": "placeholder3@emailplaceholder.com" },
-        ],
+class Friend {
+
+    constructor(id, {firstName, lastName, gender, language, email}) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.language = language;
+        this.email = email;
     }
-} };
+
+}
+
+// Temporary in-memory database
+const friendDatabase = {}
+
+const root = { 
+    friend: () => {
+        return {
+            "id": 1223,
+            "firstName": "Naman",
+            "lastName": "Maheshwari",
+            "gender": "Male",
+            "language": "English",
+            "email": "placeholder1@emailplaceholder.com",
+        }
+    },
+    createFriend: ({input}) => {
+        let id = require('crypto').randomBytes(10).toString('hex');
+        friendDatabase[id] = input;
+        return new Friend(id, input);
+    }
+};
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
