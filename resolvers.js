7e05b4d4cb5a7e5b88dabe76a24805b30b6b9c12
Naman Maheshwari <naman1901@gmail.com>
2019-01-22 19:59:04 +0530

    Refactoring code for graphql-tools and DB persistence

--- resolvers.js
+++ resolvers.js
@@ -16,15 +16,18 @@ class Friend {
 // Temporary in-memory database
 const friendDatabase = {}
 
-const resolvers = { 
-    getFriend: ({ id }) => {
-        return new Friend(id, friendDatabase[id]);
+// Resolver Map
+export const resolvers = { 
+    Query: {
+        getFriend: ({ id }) => {
+            return new Friend(id, friendDatabase[id]);
+        },
     },
-    createFriend: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        friendDatabase[id] = input;
-        return new Friend(id, input);
-    }
+    Mutation: {
+        createFriend: ({input}) => {
+            let id = require('crypto').randomBytes(10).toString('hex');
+            friendDatabase[id] = input;
+            return new Friend(id, input);
+        },
+    },
 };
-
-export default resolvers
